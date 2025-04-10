6ef12e3d16f6fd388b1cc87b1569a5e6773ad8d4
alrightCha <ychamam7@gmail.com>
2025-04-10 02:42:58 +0200

    fixed

--- src/solana/helpers.rs
+++ src/solana/helpers.rs
@@ -275,7 +275,7 @@ pub async fn get_sol_amount(amount: u64, mint: String) -> u64 {
         }
     } else {
         let price = pool_info.sell_price;
-        let sol_amount = (price * amount) / 100; //Returns amount in lamports 
+        let sol_amount = (price * amount) / 100_000; //Returns amount in lamports 100_000 because sell_price is per 100 000 tokens 
         amount_sol = sol_amount;
     }
     amount_sol
